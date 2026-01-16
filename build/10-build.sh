8a032edcc463bc4f2692fd6ee70daa703f520b1e
shaun <shaun@snotra.cloud>
2026-01-16 20:33:56 +0000

    disabling services, home folder shortcut

--- build/10-build.sh
+++ build/10-build.sh
@@ -117,6 +117,36 @@ echo "::group:: System Configuration"
 systemctl --global enable podman-auto-update.timer
 # Example: systemctl mask unwanted-service
 
+echo "Disabling print services"
+systemctl disable cups.socket
+systemctl mask cups.socket
+systemctl disable cups.service
+systemctl mask cups.service
+systemctl disable cups-browsed.service
+systemctl mask cups-browsed.service
+
+echo "Disabling avahi-daemon"
+systemctl disable avahi-daemon.socket
+systemctl mask avahi-daemon.socket
+systemctl disable avahi-daemon.service
+systemctl mask avahi-daemon.service
+
+echo "Disabling the modem manager"
+systemctl disable ModemManager.service
+systemctl mask ModemManager.service
+
+echo "Disabling the sssd daemons"
+systemctl disable sssd.service
+systemctl mask sssd.service
+systemctl disable sssd-kcm.service
+systemctl mask sssd-kcm.service
+systemctl disable sssd-kcm.socket
+systemctl mask sssd-kcm.socket
+
+echo "Disabling the location service"
+systemctl disable geoclue.service
+systemctl mask geoclue.service
+
 echo "::endgroup::"
 
 echo "::group:: Clean Up"
